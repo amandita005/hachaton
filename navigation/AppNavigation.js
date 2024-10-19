c89fd22144a6bb97d5de4b4c8979b1d2d7eded9c
Amanda <aa.ffleckk@gmail.com>
2024-10-19 12:33:25 -0300

    mapa e atualização de localização

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,13 +2,15 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LocPermissions from "../screens/LocPermissions";
+import MapComponent from "../screens/map";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation(){
     return (
         <NavigationContainer> 
-            <Stack.Navigator initialRouteName='LocPermissions'> 
+            <Stack.Navigator initialRouteName='Map'> 
+                <Stack.Screen name="Map" options={{ headerShown: false }} component={MapComponent} />
                  <Stack.Screen name="LocPermissions" options={{ headerShown: false }} component={LocPermissions} />
             </Stack.Navigator>
         </NavigationContainer>
