3dd68671406018ca1770b32b21aa01fe478dd1b9
Amanda <aa.ffleckk@gmail.com>
2024-10-20 09:34:48 -0300

    tela inicial

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -3,15 +3,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LocPermissions from "../screens/LocPermissions";
 import MapComponent from "../screens/map";
-
+import PullUpPanel from "../screens/teste";
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation(){
     return (
         <NavigationContainer> 
-            <Stack.Navigator initialRouteName='Map'> 
+            <Stack.Navigator initialRouteName='PullUpPane'> 
                 <Stack.Screen name="Map" options={{ headerShown: false }} component={MapComponent} />
                  <Stack.Screen name="LocPermissions" options={{ headerShown: false }} component={LocPermissions} />
+                 <Stack.Screen name="PullUpPane" options={{ headerShown: false }} component={PullUpPanel} />
             </Stack.Navigator>
         </NavigationContainer>
     )
